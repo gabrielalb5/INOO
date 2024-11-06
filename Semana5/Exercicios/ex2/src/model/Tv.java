0b26cbe33ad58ba8f70abe4f5ca0466633376760
gabrielalb5 <gabrielalbino28@gmail.com>
2024-11-06 12:46:58 -0300

    tv

--- Semana5/Exercicios/ex2/src/model/Tv.java
+++ Semana5/Exercicios/ex2/src/model/Tv.java
@@ -1,11 +1,54 @@
 package model;
 
 public class Tv {
-    private boolean ligada;
-    private String canal;
-    private int volume;
+    private boolean ligada = false;
+    private String canal = "Sem sinal";
+    private int volume = 0;
 
-    public void ligar(){
+    public String getCanal() {
+        return canal;
+    }
+    public int getVolume() {
+        return volume;
+    }
+    public boolean getLigada() {
+        return ligada;
+    }
 
+    public void ligar(boolean ligada){
+        this.ligada = ligada;
+        if(ligada)
+            System.out.println("A tv foi ligada");
+        else
+            System.out.println("A tv foi desligada");
+    }
+
+    public void ligar(boolean ligada, String canal){
+        this.ligada = ligada;
+        this.canal = canal;
+        if(ligada)
+            System.out.println("A tv foi ligada e sintonizada no canal "+canal);
+        else
+            System.out.println("A tv foi desligada");
+    }
+
+    public void ligar(boolean ligada, String canal, int volume){
+        this.ligada = ligada;
+        this.canal = canal;
+        this.volume = volume;
+        if(ligada)
+            System.out.println("A tv foi ligada e sintonizada no canal "+canal+" - volume: "+volume);
+        else
+            System.out.println("A tv foi desligada");
+    }
+
+    public static void getStatus(Tv televisao) {
+        System.out.println("STATUS DA TV");
+        if(televisao.getLigada())
+            System.out.println("Ligada");
+        else
+           System.out.println("Desligada"); 
+        System.out.println(televisao.getCanal()); 
+        System.out.println("Volume: "+televisao.getVolume()+"\n"); 
     }
 }
--- Semana5/Exercicios/ex2/src/view/ControleRemoto.java
+++ Semana5/Exercicios/ex2/src/view/ControleRemoto.java
@@ -5,5 +5,19 @@ import model.Tv;
 public class ControleRemoto {
     public static void main(String[] args) {
         Tv televisao = new Tv();
+
+        Tv.getStatus(televisao);
+
+        televisao.ligar(true);
+        Tv.getStatus(televisao);
+
+        televisao.ligar(true, "Cartoon Network");
+        Tv.getStatus(televisao);
+
+        televisao.ligar(true, "Nickelodeon", 20);
+        Tv.getStatus(televisao);
+
+        televisao.ligar(false);
+        Tv.getStatus(televisao);
     }
 }

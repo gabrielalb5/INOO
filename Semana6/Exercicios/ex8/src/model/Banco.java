37bace8dfff06cac6f6401204225358f4f0c14d7
gabrielalb5 <gabrielalbino28@gmail.com>
2024-11-06 12:02:54 -0300

    banco

--- Semana6/Exercicios/ex8/src/model/Banco.java
+++ Semana6/Exercicios/ex8/src/model/Banco.java
@@ -1,5 +1,14 @@
 package model;
 
 public class Banco {
-    
+    private static final double taxaJuros = 10.0;
+    private static final double CDI = 11.20;
+
+    public static double rendimentoCDB(double deposito){
+        return deposito+deposito*(CDI/100);
+    }
+
+    public static double getTaxajuros() {
+        return taxaJuros;
+    }
 }
--- Semana6/Exercicios/ex8/src/view/Main.java
+++ Semana6/Exercicios/ex8/src/view/Main.java
@@ -1,5 +1,13 @@
 package view;
 
+import model.Banco;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        double deposito = 7000.00;
+        double rendimento = Banco.rendimentoCDB(deposito);
+        System.out.println("Total R$"+rendimento);
+        System.out.println("Seu depósito de R$"+deposito+" rendeu "+(rendimento-deposito));
+        System.out.println("A taxa de "+Banco.getTaxajuros()+"% implicou em um desconto de "+((rendimento-deposito)*(Banco.getTaxajuros()/100)));
+    }
 }

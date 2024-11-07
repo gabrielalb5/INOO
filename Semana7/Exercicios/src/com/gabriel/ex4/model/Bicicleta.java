dc85a277ac44e065114e63dd214d2db6ecf7157e
gabrielalb5 <gabrielalbino28@gmail.com>
2024-11-07 12:28:55 -0300

    bicicleta

--- Semana7/Exercicios/src/com/gabriel/ex4/model/Bicicleta.java
+++ Semana7/Exercicios/src/com/gabriel/ex4/model/Bicicleta.java
@@ -2,13 +2,49 @@ package com.gabriel.ex4.model;
 
 public class Bicicleta {
     private int id;
-    private String modelo;
-    private boolean disponivel;
+    private String cor;
+    private boolean disponivel = true;
     private String ultimaReserva = null;
+    private double precoAluguel;
 
-    public Bicicleta(int id, String modelo, boolean disponivel){
+    public Bicicleta(int id, String cor, double precoAluguel){
         this.id = id;
-        this.modelo = modelo;
+        this.cor = cor;
+        this.precoAluguel = precoAluguel;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public double getPrecoAluguel() {
+        return precoAluguel;
+    }
+
+    public String getUltimaReserva() {
+        return ultimaReserva;
+    }
+    
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    public void setDisponivel(boolean disponivel) {
         this.disponivel = disponivel;
+    }
+
+    public void setUltimaReserva(String ultimaReserva) {
+        this.ultimaReserva = ultimaReserva;
+    }
+
+    public void relatorioBicicleta(){
+        if (disponivel)
+            System.out.println(getId()+" - "+getCor()+" está disponível | "+getPrecoAluguel()+" pontos | última reserva: "+getUltimaReserva());
+        else
+            System.out.println(getId()+" - "+getCor()+" está indisponível | "+getPrecoAluguel()+" pontos | última reserva: "+getUltimaReserva());
     }
 }
--- Semana7/Exercicios/src/com/gabriel/ex4/model/Usuario.java
+++ Semana7/Exercicios/src/com/gabriel/ex4/model/Usuario.java
@@ -4,12 +4,76 @@ public class Usuario {
     private String nome;
     private String cpf;
     private String email;
-    private String bicicletaReservada = null;
-    private int saldoCreditos = 0;
+    private Bicicleta bicicletaReservada = null;
+    private double saldoCreditos = 0;
 
     public Usuario(String nome, String cpf, String email){
         this.nome = nome;
         this.cpf = cpf;
         this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public double getSaldoCreditos() {
+        return saldoCreditos;
+    }
+
+    public void creditar(int credito){
+        if(credito>0){
+            this.saldoCreditos = credito;
+            System.out.println(getNome()+" foi creditado com "+credito+" pontos");
+        }else{
+            System.out.println("Valor de crédito inválido");
+        }
+    }
+
+    public void reservarBicicleta(Bicicleta bicicleta){
+        if(this.bicicletaReservada == null){
+            if(bicicleta.isDisponivel()){
+                if(this.saldoCreditos >= bicicleta.getPrecoAluguel()){
+                    this.bicicletaReservada = bicicleta;
+                    bicicleta.setDisponivel(false);
+                    bicicleta.setUltimaReserva(getCpf());
+                    this.saldoCreditos = this.saldoCreditos-bicicleta.getPrecoAluguel();
+                    System.out.println(getNome()+" emprestou a bicicleta \""+bicicleta.getCor()+"\" com sucesso!");
+                }else{
+                    System.out.println("ERRO: "+getNome()+", você não tem créditos suficientes para emprestar a bicicleta \""+bicicleta.getCor()+"\"");
+                    System.out.println("Seus pontos: "+getSaldoCreditos()+" | Bicicleta: "+bicicleta.getPrecoAluguel());
+                }
+            }else{
+                System.out.println("ERRO: A bicicleta \""+bicicleta.getCor()+"\" não está disponível");
+            }
+        }else{
+            System.out.println("ERRO: "+getNome()+", devolva uma bicicleta antes de emprestar outra");
+        }
+    }
+
+    public void devolverBicicleta(){
+        if(bicicletaReservada == null){
+            System.out.println(getNome()+", você não tem nenhuma bicicleta emprestada");
+        }else{
+            System.out.println(getNome()+" devolveu a bicicleta \""+bicicletaReservada.getCor()+"\"");
+            bicicletaReservada.setDisponivel(true);
+            bicicletaReservada = null;
+        }
+    }
+
+    public void relatorioUsuario(){
+        if(bicicletaReservada == null)
+            System.out.println(getNome()+" ("+getCpf()+") - Tem "+getSaldoCreditos()+" pontos e está sem bicicleta");
+        else
+            System.out.println(getNome()+" ("+getCpf()+") - Tem "+getSaldoCreditos()+" pontos e está com a bicicleta \""+bicicletaReservada.getCor()+"\"");
+        
     }
 }
